d3b52eb26f44b841f95924e315d5dcb1cc255347
Raed Ali Elgamal <raed.aljamal@gizasystems.onmicrosoft.com>
2024-12-16 14:33:38 +0200

    Add files via upload
    Committing src and pom file

---
+++ src/main/java/utilitiles/DriverFactory.java
@@ -0,0 +1,25 @@
+package utilitiles;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+    static WebDriver driver;
+
+    public static WebDriver initiateDriver(String browserName, boolean maximize) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            System.out.println("initializing Chrome Browser on OS: " + System.getProperty("os.name") + " and it's version is: " + System.getProperty("os.version"));
+            driver = new ChromeDriver();
+            System.out.println("Successfully initialized the Chrome Browser");
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        }
+        if (maximize) {
+            driver.manage().window().maximize();
+            System.out.println("Window is maximized. And the new window size is now: " + driver.manage().window().getSize());
+        }
+        return driver;
+
+    }
+}
